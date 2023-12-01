f1fc59d0e12dcbd440ac94f6e209b464e12d9e1f
Vhyrro <vhyrro@gmail.com>
2023-12-01 19:06:27 +0100

    feat: finish ontology implementation

--- src/ontology/mod.rs
+++ src/ontology/mod.rs
@@ -5,44 +5,44 @@
 //! - Allow transferring data between ontologies and even completely changing the view
 //!   of a given ontology (with proper type checking).
 
-use std::{collections::HashMap, marker::PhantomData, path::PathBuf};
+use std::{collections::HashMap, path::PathBuf};
 
-pub trait View {
-    type Structure;
+/// A very generic trait of a View.
+/// Views allow an [OpaqueOntology]'s contents to be examined and retrieved in a structured manner.
+pub trait View {}
 
-    fn display<D: OpaqueOntology<Self>>(ontology: &Ontology<Self, D>) -> Self::Structure
-    where
-        Self: Sized;
+/// A trait to display the contents of an Ontology in the form of a list of items.
+pub trait ListView<V: View, D: OpaqueOntology<V, Derives = D>, S: OpaqueOntology<V, Derives = D>>:
+    View
+{
+    fn display(&self) -> S::Structure;
 }
 
-struct ListView {}
-
-impl View for ListView {
-    type Structure = Vec<Item>;
-
-    fn display<D: OpaqueOntology<Self>>(ontology: &Ontology<Self, D>) -> Self::Structure {
-        ontology.items.clone()
-    }
-}
-
-pub trait OpaqueOntology<V: View> {
+/// A generic trait of an ontology. The trait is "opaque" because it contains no
+/// physical data, only the general structure of an ontology.
+///
+/// Ontologies are collections of information about a given topic. Examples of ontologies include
+/// but are not limited to:
+/// - Lists of upcoming tasks
+/// - Graphs of connections between notes
+/// - Time-sensitive calendars
+///
+/// All of these are representable via this trait.
+///
+/// Ontologies may derive from other ontologies if both have the same view (i.e. the same data layout).
+pub trait OpaqueOntology<V: View + ?Sized> {
     type Derives: OpaqueOntology<V>;
+    type Structure;
 }
 
+// Since every ontology must derive from something, here we define an identity ontology. Such an
+// ontology has no structure and simply derives from itself.
 impl<V: View> OpaqueOntology<V> for () {
+    type Structure = ();
     type Derives = Self;
 }
 
-pub struct Ontology<V: View, D: OpaqueOntology<V>> {
-    items: Vec<Item>,
-    phantom_view: PhantomData<V>,
-    phantom_derives: PhantomData<D>,
-}
-
-impl<V: View, D: OpaqueOntology<V>> OpaqueOntology<V> for Ontology<V, D> {
-    type Derives = D;
-}
-
+/// A unit of data for general metadata.
 #[derive(Clone)]
 enum Metadata {
     Date(chrono::NaiveDate),
@@ -56,7 +56,7 @@ enum Metadata {
 /// * `metadata`: A key : value mapping. Any metadata can be bound to any other metadata if
 ///               necessary.
 #[derive(Clone)]
-struct Item {
+pub struct Item {
     text: String,
     metadata: HashMap<Metadata, Metadata>,
 }
